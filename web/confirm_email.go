359c7d32463161409b5640f2d409979d7559a260
Richard Knop <risoknop@gmail.com>
2016-03-06 01:09:18 +0800

    refactored confirm email handler.

--- web/confirm_email.go
+++ web/confirm_email.go
@@ -5,13 +5,6 @@ import (
 )
 
 func (s *Service) confirmEmail(w http.ResponseWriter, r *http.Request) {
-	// Get the session service from the request context
-	sessionService, err := getSessionService(r)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
 	// Get the confirmation from the request context
 	confirmation, err := getConfirmation(r)
 	if err != nil {
@@ -20,15 +13,13 @@ func (s *Service) confirmEmail(w http.ResponseWriter, r *http.Request) {
 	}
 
 	// Confirm the email
+	var confirmationErr string
 	if err := s.accountsService.ConfirmUser(confirmation.User); err != nil {
-		sessionService.SetFlashMessage(err.Error())
-		http.Redirect(w, r, r.RequestURI, http.StatusFound)
-		return
+		confirmationErr = err.Error()
 	}
 
 	// Render the template
-	errMsg, _ := sessionService.GetFlashMessage()
 	renderTemplate(w, "confirm-email.html", map[string]interface{}{
-		"error": errMsg,
+		"error": confirmationErr,
 	})
 }
