abfabaca15ab95144b9c3cca959ff18d9d899393
Richard Knop <RichardKnop@users.noreply.github.com>
2016-05-18 19:27:05 +0800

    Update login_test.go

--- facebook/login_test.go
+++ facebook/login_test.go
@@ -365,3 +365,86 @@ func (suite *FacebookTestSuite) TestLoginCreatesNewUser() {
 		assert.Equal(suite.T(), string(expected), strings.TrimSpace(w.Body.String()))
 	}
 }
+
+func (suite *FacebookTestSuite) TestLoginCreatesNewUserNilEmail() {
+	// Prepare a request
+	r, err := http.NewRequest("POST", "http://1.2.3.4/v1/facebook/login", nil)
+	assert.NoError(suite.T(), err, "Request setup should not get an error")
+	r.PostForm = url.Values{
+		"access_token": {"facebook_token"},
+		"scope":        {"read_write"},
+	}
+	r.SetBasicAuth("test_client_1", "test_secret")
+
+	// Check the routing
+	match := new(mux.RouteMatch)
+	suite.router.Match(r, match)
+	if assert.NotNil(suite.T(), match.Route) {
+		assert.Equal(suite.T(), "facebook_login", match.Route.GetName())
+	}
+
+	// Mock fetching profile data from facebook
+	suite.mockFacebookGetMe(fb.Result{
+		"id":         interface{}("new_facebook_id"),
+		"email":      interface{}(nil),
+		"first_name": interface{}("John"),
+		"last_name":  interface{}("Reese"),
+	}, nil)
+
+	// Count before
+	var countBefore int
+	suite.db.Model(new(accounts.User)).Count(&countBefore)
+
+	// And serve the request
+	w := httptest.NewRecorder()
+	suite.router.ServeHTTP(w, r)
+
+	// Check mock expectations were met
+	suite.adapterMock.AssertExpectations(suite.T())
+
+	// Check the status code
+	if !assert.Equal(suite.T(), 200, w.Code) {
+		log.Print(w.Body.String())
+	}
+
+	// Count after
+	var countAfter int
+	suite.db.Model(new(accounts.User)).Count(&countAfter)
+	assert.Equal(suite.T(), countBefore+1, countAfter)
+
+	// Fetch the created user
+	user := new(accounts.User)
+	notFound := suite.db.Preload("Account").Preload("OauthUser").
+		Preload("Role.Permissions").Last(user).RecordNotFound()
+	assert.False(suite.T(), notFound)
+
+	// And correct data was saved
+	assert.Equal(suite.T(), user.ID, user.OauthUser.MetaUserID)
+	assert.Equal(suite.T(), "new_facebook_id@facebook.com", user.OauthUser.Username)
+	assert.Equal(suite.T(), "John", user.FirstName.String)
+	assert.Equal(suite.T(), "Reese", user.LastName.String)
+	assert.Equal(suite.T(), "new_facebook_id", user.FacebookID.String)
+	assert.Equal(suite.T(), roles.User, user.Role.ID)
+	assert.True(suite.T(), user.Confirmed)
+
+	// Fetch oauth tokens
+	accessToken := new(oauth.AccessToken)
+	assert.False(suite.T(), suite.db.Preload("Client").Preload("User").
+		First(accessToken).RecordNotFound())
+	refreshToken := new(oauth.RefreshToken)
+	assert.False(suite.T(), suite.db.Preload("Client").Preload("User").
+		First(refreshToken).RecordNotFound())
+
+	// Check the response body
+	expected, err := json.Marshal(&oauth.AccessTokenResponse{
+		UserID:       user.ID,
+		AccessToken:  accessToken.Token,
+		ExpiresIn:    3600,
+		TokenType:    oauth.TokenType,
+		Scope:        "read_write",
+		RefreshToken: refreshToken.Token,
+	})
+	if assert.NoError(suite.T(), err, "JSON marshalling failed") {
+		assert.Equal(suite.T(), string(expected), strings.TrimSpace(w.Body.String()))
+	}
+}
