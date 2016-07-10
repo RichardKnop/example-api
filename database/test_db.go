68da5a112d7ea7a135ea2a9bd56e326bb71a9715
Richard Knop <risoknop@gmail.com>
2016-07-11 00:34:55 +0800

    Database test utils and unit tests.

--- database/test_db.go
+++ database/test_db.go
@@ -74,18 +74,17 @@ func rebuildDatabase(dbPath string) (*gorm.DB, error) {
 // rebuildDatabase attempts to delete an existing Postgres
 // database and rebuild it, returning a pointer to it
 func rebuildDatabasePostgres(dbUser, dbName string) (*gorm.DB, error) {
-	// Delete the current database if it exists
-	dropDbCmd := fmt.Sprintf("dropdb -U %s %s", dbUser, dbName)
-	exec.Command("sh", "-c", dropDbCmd).Output()
 
-	// Create a new test database
-	createDbCmd := fmt.Sprintf("createdb -U %s %s", dbUser, dbName)
-	out, err := exec.Command("sh", "-c", createDbCmd).Output()
-	if err != nil {
-		log.Print(out)
+	dropPostgresDB(dbUser, dbName)
+
+	if err := createPostgresDB(dbUser, dbName); err != nil {
 		return nil, err
 	}
 
+	return openPostgresDB(dbUser, dbName)
+}
+
+func openPostgresDB(dbUser, dbName string) (*gorm.DB, error) {
 	// Init a new postgres test database connection
 	db, err := gorm.Open("postgres",
 		fmt.Sprintf(
@@ -98,4 +97,23 @@ func rebuildDatabasePostgres(dbUser, dbName string) (*gorm.DB, error) {
 		return nil, err
 	}
 	return db, nil
+}
+
+func createPostgresDB(dbUser, dbName string) error {
+	// Create a new test database
+	createDbCmd := fmt.Sprintf("createdb -U %s %s", dbUser, dbName)
+	log.Println(createDbCmd)
+	out, err := exec.Command("sh", "-c", createDbCmd).Output()
+	if err != nil {
+		log.Printf("%v", string(out))
+		return err
+	}
+	return nil
+}
+
+func dropPostgresDB(dbUser, dbName string) {
+	// Delete the current database if it exists
+	dropDbCmd := fmt.Sprintf("dropdb --if-exists -U %s %s", dbUser, dbName)
+	fmt.Println(dropDbCmd)
+	exec.Command("sh", "-c", dropDbCmd).Output()
 }
